2836de71d78964906ab9d242b6177c2e81b2b71b
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2020-01-02 20:41:51 +0200

    Updates

---
+++ precache-manifest.3c3082277d17a8df5c7e60c8e8ed5ec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fe083024cddb963406cf5acccc502c",
+    "url": "/starDB/index.html"
+  },
+  {
+    "revision": "bfa23936b4e31eff75b4",
+    "url": "/starDB/static/css/main.285ffa95.chunk.css"
+  },
+  {
+    "revision": "a97c0854857959b63ad4",
+    "url": "/starDB/static/js/2.22d1dc79.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starDB/static/js/2.22d1dc79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfa23936b4e31eff75b4",
+    "url": "/starDB/static/js/main.6b7669ae.chunk.js"
+  },
+  {
+    "revision": "65c1febcdbd721235906",
+    "url": "/starDB/static/js/runtime-main.d04c3101.js"
+  }
+]);
